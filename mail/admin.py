2d898883d4ec14d29faa8db697f728c0fefca565
Shun <109353349+Evangelistsb@users.noreply.github.com>
2022-08-10 20:23:31 +0100

    Update admin.py

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+# NOTE: Please only update this file when necessary.
+# Check the official django docs for more info on how to update this file
